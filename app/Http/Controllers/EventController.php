0571c5f91eddf5550526c552c1b694c63b9f9c81
jose <josemh@hotmail.com>
2024-01-17 20:34:46 +0100

    Añadido tags, columna event_tag y pintada vista index

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,12 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateEventRequest;
-use App\Http\Requests\UpdateEventRequest;
 use App\Models\Country;
 use App\Models\Event;
 use App\Models\Tag;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Illuminate\View\View;
@@ -20,8 +18,8 @@ class EventController extends Controller
      */
     public function index(): View
     {
-        $events = Event::with('country')->get();
-        return view('events.index', compact('events'));
+        $events = Event::with('country')->get(); // Guardo el modelo. With para la relacion. Problema N+1
+        return view('events.index', compact('events')); // Devuelvo vista
     }
 
     /**
@@ -31,14 +29,28 @@ class EventController extends Controller
     {
         $countries = Country::all();
         $tags = Tag::all();
-        return view('events.create', compact('countries', 'tags'));
+        return view('events.create', compact('countries', 'tags')); // compact() permite usar las variables en la vista.
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store()
+    public function store(CreateEventRequest $request): RedirectResponse
     {
+        if ($request->hasFile('image')) {
+
+            $data = $request->validated(); // Almacena los datos del formRequest
+            $data['image'] = Storage::putFile('events', $request->file('image')); // Sube la imagen y la guarda en la BD
+            $data['user_id'] = auth()->id(); // Asigna el id del usuario al campo user_id
+            $data['slug'] = Str::slug($request->title); // Idem
+
+            $event = Event::create($data); // Asigna los datos al modelo Event y lo crea en la BD
+            $event->tags()->attach($request->tags);
+
+            return to_route('events.index'); // Redireciona al crear evento
+        } else {
+            return back();
+        }
     }
 
 
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('New Event') }}
+                {{ __('Nuevo Evento') }}
             </h2>
         </div>
     </x-slot>
@@ -130,7 +130,7 @@
                         @enderror
                     </div>
                 </div>
-                {{-- <div>
+                <div>
                     <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Tags</h3>
                     <ul
                         class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
@@ -146,7 +146,7 @@
                             </li>
                         @endforeach
                     </ul>
-                </div> --}}
+                </div>
                 <div>
                     <button type="submit"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create</button>
